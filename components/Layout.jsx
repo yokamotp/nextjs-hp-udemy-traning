80fba43785b3f599845079a8d2d6bc547962b8d7
Yoppey <apple@appurunoMacBook-Pro.local>
2023-01-14 18:22:59 +0900

    Finish lesson 10

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -32,6 +32,18 @@ export default function Layout({ children, title = "HP by Nextjs"}) {
         </nav>
       </header>
       <main className="flex flex-1 justify-center items-center flex-col w-screen">{children}</main>
+      <footer className="w-full h-12 flex justify-center items-center border-t">
+        <a
+          className="flex items-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          {/* <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" /> */}
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </a>
+      </footer>
     </div>
 );
 
